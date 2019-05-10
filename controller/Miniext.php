84652e7cb7aedaa2aabf141e60eb5dd67fd047fc
janfix <jean.philippe.riviere@gmail.com>
2019-05-10 15:13:26 +0200

    class-session-manager

--- controller/Miniext.php
+++ controller/Miniext.php
@@ -21,6 +21,15 @@
 
 namespace Wiquid\miniext\controller;
 
+use core_kernel_classes_Class;
+use oat\generis\model\user\UserRdf;
+use oat\oatbox\PhpSerializable;
+use oat\tao\model\accessControl\ActionResolver;
+use oat\oatbox\user\User;
+use oat\taoOutcomeUi\controller\Results;
+use oat\taoOutcomeUi\model\ResultsService;
+
+//print_r(get_declared_classes());
 /**
  * Sample controller
  *
@@ -29,6 +38,7 @@ namespace Wiquid\miniext\controller;
  * @license GPL-2.0
  *
  */
+
 class Miniext extends \tao_actions_CommonModule {
 
     /**
@@ -42,11 +52,60 @@ class Miniext extends \tao_actions_CommonModule {
      * A possible entry point to tao
      */
     public function index() {
-        echo __("Hello World");
+        
+        
     }
 
     public function templateExample() {
-        $this->setData('author', 'JPR');
+       
+        
+        $class_methods = get_class_methods(\common_Session_SessionManager::getSession());
+        Echo "<br>Comment avoir les methodes d'une classe ?<br>";
+        foreach ($class_methods as $method_name) {
+            echo "$method_name.<br>";
+        }
+
+
+        $user = \common_Session_SessionManager::getSession()->getUser();
+        $classUri = $user->getPropertyValues(UserRdf::CLASS_URI);
+        $login = $user->getPropertyValues(UserRdf::PROPERTY_LOGIN);
+        $password = $user->getPropertyValues(UserRdf::PROPERTY_PASSWORD);
+        $firstName = $user->getPropertyValues(UserRdf::PROPERTY_FIRSTNAME);
+        $lastName = $user->getPropertyValues(UserRdf::PROPERTY_LASTNAME);
+        $uilg = $user->getPropertyValues(UserRdf::PROPERTY_UILG);
+        $deflg = $user->getPropertyValues(UserRdf::PROPERTY_DEFLG);
+        $mail = $user->getPropertyValues(UserRdf::PROPERTY_MAIL);
+        $firstName = $user->getPropertyValues(UserRdf::PROPERTY_FIRSTNAME);
+        $lastName = $user->getPropertyValues(UserRdf::PROPERTY_LASTNAME);
+        $role = $user->getPropertyValues(UserRdf::PROPERTY_ROLES);
+        $timeZone = $user->getPropertyValues(UserRdf::PROPERTY_TIMEZONE);
+
+        $userUri = \common_Session_SessionManager::getSession()->getUserUri();
+        //var_dump($userUri);
+
+        /* echo $logJP[0]."<br>";
+        echo $FNJP[0]."<br>";
+        echo $LNJP[0]."<br>"; */
+
+        $this->setData('firstName', $firstName[0]);
+        //$this->setData('classUri',$classUri[0]);
+        $this->setData('login',$login[0]);
+        $this->setData('password',$password[0]);
+        $this->setData('firstName',$firstName[0]);
+        $this->setData('lastName',$lastName[0]);
+        $this->setData('uilg',$uilg[0]);
+        $this->setData('deflg',$deflg[0]);
+        $this->setData('mail',$mail[0]);
+        $this->setData('firstName',$firstName[0]);
+        $this->setData('lastName',$lastName[0]);
+        $this->setData('role',$role[0]);
+        $this->setData('timeZone',$timeZone[0]);
         $this->setView('Miniext/templateExample.tpl');
+    }
+
+     public function templatebis() {
+       
+        $this->setData('author', "Toto");
+        $this->setView('Miniext/templatebis.tpl');
     }
 }
---
+++ views/templates/Miniext/templatebis.tpl
@@ -0,0 +1,20 @@
+<div class="main-container tao-scope">
+    <h1>Salut <?=(get_data('author'))?></h1>
+    Alors ça va ?  <?=(get_data('author'))?>
+<ul>
+<li><?=(get_data('firstName'))?></li>
+<li><?=(get_data('classUri'))?></li>
+<li><?=(get_data('login'))?></li>
+<li><?=(get_data('password'))?></li>
+<li><?=(get_data('firstName'))?></li>
+<li><?=(get_data('lastName'))?></li>
+<li><?=(get_data('uilg'))?></li>
+<li><?=(get_data('deflg'))?></li>
+<li><?=(get_data('mail'))?></li>
+<li><?=(get_data('firstName'))?></li>
+<li><?=(get_data('lastName'))?></li>
+<li><?=(get_data('role'))?></li>
+<li><?=(get_data('timeZone'))?></li>
+</ul>
+
+</div>
